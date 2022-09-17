a9ae92dcd76e83d2cd6b6c6884b8739273bc5962
NirmalGit <nirsha2007@gmail.com>
2022-09-17 07:41:13 +0530

    feat: prettier

--- src/app/books/books.module.ts
+++ src/app/books/books.module.ts
@@ -3,15 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { BooksRoutingModule } from './books-routing.module';
 import { HomeComponent } from './home/home.component';
-
+import { AgGridModule } from 'ag-grid-angular';
 
 @NgModule({
-  declarations: [
-    HomeComponent
-  ],
-  imports: [
-    CommonModule,
-    BooksRoutingModule
-  ]
+  declarations: [HomeComponent],
+  imports: [CommonModule, AgGridModule, BooksRoutingModule],
 })
-export class BooksModule { }
+export class BooksModule {}
--- src/app/books/home/home.component.ts
+++ src/app/books/home/home.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { ColDef } from 'ag-grid-community';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
+  rowData: any[] = [
+    { make: 'Toyota', model: 'etios', price: 5000 },
+    { make: 'Hyundai', model: 'i20', price: 6000 },
+    { make: 'Maruti', model: 'baleno', price: 4000 },
+  ];
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  colDefs: ColDef[] = [
+    { field: 'make' },
+    { field: 'model' },
+    { field: 'price' },
+  ];
 }
